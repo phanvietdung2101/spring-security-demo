d2c21b07f8d1c41a647b4c7ac414813b65272f5f
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-05-15 14:23:16 +0700

    done demo

---
+++ src/main/java/com/codegym/MvcWebApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.codegym;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { WebSecurityConfig.class , WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
